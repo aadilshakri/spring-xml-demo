6f07ff51d0bbd7aa1eff11dd21803b6f8388a1e0
aadilshakri <aadilshakri@gmail.com>
2019-10-09 11:00:11 +0530

    task2

--- src/main/java/com/stackroute/App.java
+++ src/main/java/com/stackroute/App.java
@@ -16,8 +16,9 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
-        Movie movieInfo = context.getBean("movies", Movie.class);
-//        Actor ActorInfo=context.getBean("actors", Actor.class);
-        System.out.println(movieInfo.getActor().getAge());
+        Movie movieInfo = context.getBean("MovieA", Movie.class);
+        Movie movieInfo1 = context.getBean("MovieB", Movie.class);
+        movieInfo.getActor().displayInfo();
+        System.out.println(movieInfo==movieInfo1);
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,13 +1,20 @@
 package com.stackroute.domain;
 
 public class Movie {
-     Actor actor;
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
+//    public Movie() {
+//    }
+
+    private Actor actor;
 
     public Actor getActor() {
         return actor;
     }
-
-    public void setActor(Actor actor) {
-        this.actor = actor;
-    }
+//
+//    public void setActor(Actor actor) {
+//        this.actor = actor;
+//    }
 }
