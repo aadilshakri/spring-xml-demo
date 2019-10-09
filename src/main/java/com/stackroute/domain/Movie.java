808203c57d4eb9b29cc6fba0a89e568881548012
aadilshakri <aadilshakri@gmail.com>
2019-10-09 14:50:08 +0530

    aware interface

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,9 @@
 package com.stackroute.domain;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-public class Movie {
+public class Movie implements ApplicationContextAware {
 
     public Movie() {
     }
@@ -8,9 +11,6 @@ public class Movie {
     public Movie(Actor actor) {
         this.actor = actor;
     }
-
-
-
     private Actor actor;
 
     public Actor getActor() {
@@ -19,5 +19,17 @@ public class Movie {
 //
     public void setActor(Actor actor) {
         this.actor = actor;
+    }
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+        System.out.println(applicationContext.toString());
+    }
+
+    public ApplicationContext getApplicationContext(){
+        return applicationContext;
     }
 }
